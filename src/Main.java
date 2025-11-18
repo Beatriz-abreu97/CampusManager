225befae6731c7a95ddcdac895a964df6e65c667
Vagner Moura <138622443+VagnerMouraMacedo@users.noreply.github.com>
2025-11-17 22:04:36 -0300

    Pequenas alterações no main
    Troca de nome da classe que vai o main de PessoaTeste para Main a exclusão do método dentro da classe main

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,46 @@
-class PessoaTeste extends Pessoa {
-    public PessoaTeste(String nome, String cpf, int idade) {
-        super(nome, cpf, idade);
-    }
-}
-
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Teste q. 1 e 2(classe pessoa)");
+  public static void main(String[] args) {
+    System.out.println("Teste q. 1 e 2(classe pessoa)");
+    Aluno aluno1 = new Aluno("Beatriz Abreu", "000.000.000-00", 28, "112-3");
 
-        PessoaTeste beatriz = new PessoaTeste("Beatriz Abreu", "000.000.000-00",28);
 
-        System.out.println("Nome: " + beatriz.getNome());
-        System.out.println("Idade inicial: " + beatriz.getIdade());
-    
-        beatriz.fazerAniversario();
+    System.out.println("Nome: " + aluno1.getNome());
+    System.out.println("Idade inicial: " + aluno1.getIdade());
 
-        System.out.println("Nova idade:" + beatriz.getIdade());
+    aluno1.fazerAniversario();
 
-        System.out.println("---------------");
+    System.out.println("Nova idade:" + aluno1.getIdade());
 
-        Disciplina poo = new Disciplina("Matemática", "685","2025.2");
+    System.out.println("---------------");
 
-        System.out.println("Disciplina: " + poo.getNome());
-        System.out.println("Código: " + poo.getCodigo());
-        System.out.println("Semestre: " + poo.getSemestre());
+    Disciplina poo = new Disciplina("Matemática", "685", "2025.2");
 
-        System.out.println("---------------");
+    System.out.println("Disciplina: " + poo.getNome());
+    System.out.println("Código: " + poo.getCodigo());
+    System.out.println("Semestre: " + poo.getSemestre());
 
-        Aluno novoAluno = new Aluno("Paula", "111.111.111-11",30,"xxxx-x");
-        novoAluno.pagarMensalidade();      
+    System.out.println("---------------");
 
-        System.out.println("---------------");
-        
-        AlunoRegular novoAlunoRegular = new AlunoRegular("Maria","222.222.222-22",25,"000000-0");
-        novoAlunoRegular.pagarMensalidade();
+    Aluno novoAluno = new Aluno("Paula", "111.111.111-11", 30, "xxxx-x");
+    novoAluno.pagarMensalidade();
 
-        System.out.println("---------------");
+    System.out.println("---------------");
 
-        AlunoBolsista novoBolsista = new AlunoBolsista("João","333.333.333-33",22,"zzzz-z");
-        novoBolsista.pagarMensalidade();
+    AlunoRegular novoAlunoRegular = new AlunoRegular("Maria", "222.222.222-22", 25, "000000-0");
+    novoAlunoRegular.pagarMensalidade();
 
-        System.out.println("---------------");
+    System.out.println("---------------");
 
-        Professor novoProfessor = new Professor ("Matheus","555.555.555-55",35,"Matemática");
-        novoProfessor.darAula();
+    AlunoBolsista novoBolsista = new AlunoBolsista("João", "333.333.333-33", 22, "zzzz-z");
+    novoBolsista.pagarMensalidade();
 
-        System.out.println("---------------");
+    System.out.println("---------------");
 
-    }
-    
+    Professor novoProfessor = new Professor("Matheus", "555.555.555-55", 35, "Matemática");
+    novoProfessor.darAula();
+
+    System.out.println("---------------");
+
+  }
+
 }
