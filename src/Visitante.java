4f27625191a609b925e1d7dfc4688654a1b55d63
edenderocha-spec <edenderocha@gmail.com>
2025-11-30 19:15:08 -0300

    Update Visitante.java

--- src/Visitante.java
+++ src/Visitante.java
@@ -2,7 +2,4 @@ public class Visitante extends Pessoa {
     public Visitante(String nome, String cpf, int idade) {
         super(nome, cpf, idade);
     }
-    public void visitar() {
-            System.out.println("O(A) " + this.getNome() + " visitou a turma.");
-        }
 }
